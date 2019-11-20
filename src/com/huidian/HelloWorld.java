ca14a189a0bcaf062e5fd648048dad6298e99e25
lingweiov <lingweiov@foxmail.com>
2019-11-20 14:45:08 +0800

    Initial commit

---
+++ src/com/huidian/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.huidian;/*
+@outhor shkstart
+@date 2019/11/20-11:58
+*/
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloWOrld");
+    }
+}
